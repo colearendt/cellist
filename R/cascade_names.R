b7c7771021c7c19293c22cbb05b90184ca3165cc
Cole Arendt <cole.arendt@outlook.com>
2017-12-31 16:14:17 -0500

    Add cascade_names

---
+++ R/cascade_names.R
@@ -0,0 +1,22 @@
+#' Cascade Names
+#'
+#' Cascade names takes data_frames in a nested lists and pulls
+#' them up to the top level, appending names that it encounters
+#' along the way
+cascade_names <- function(list_or_df, prefix="") {
+  UseMethod("cascade_names")
+}
+
+cascade_names.list <- function(list_or_df, prefix="") {
+  if (is.null(names(list_or_df))) {
+    nm <- ""
+  } else {
+    nm <- names(list_or_df)
+  }
+  obj <- lapply(list_or_df, cascade_names, prefix=paste0(prefix, nm))
+  return(bind_cols(obj))
+}
+
+cascade_names.data.frame <- function(list_or_df, prefix="") {
+  return(list_or_df %>% rename_all(.funs=list(function(x){paste0(prefix,x)})))
+}
